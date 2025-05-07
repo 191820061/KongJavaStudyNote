5a61ae04bd8d6432ef96bf89751ea56067027548
191820061 <1304780263@qq.com>
2025-05-07 21:31:03 +0800

    第三天结束

---
+++ month1/week1/day3/com/example/UserTest.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class UserTest {
+    public static void main(String[] args) {
+        User user = new User("kong","191820061","1304780263","男",23);
+        System.out.println(user.getAge());
+    }
+}

73528f2c9a88d9d577d6004f55e8577bc84a786f
191820061 <1304780263@qq.com>
2025-05-21 11:35:57 +0800

    springBoot学习

---
+++ month1/week2/spring-hello-demo/src/main/java/com/kong/SpringHelloDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kong.spring_hello_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHelloDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHelloDemoApplication.class, args);
+	}
+
+}
---
+++ month1/week2/spring-hello-demo/src/test/java/com/kong/SpringHelloDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kong.spring_hello_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHelloDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
